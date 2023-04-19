4a93cbf53a5b9ae0eb79952f5677b6faf0448ad5
Bekzod <63925033+XudayberdiyevB@users.noreply.github.com>
2023-04-19 17:33:46 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["*.uzdasturchi.com", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
